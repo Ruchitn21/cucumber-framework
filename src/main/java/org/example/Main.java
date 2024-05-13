6a4cdcec6ad963f02ef2e540208846f8fbe7c031
Ruchitn21 <nigam21ruchit@gmail.com>
2024-05-13 20:41:28 +0530

    First Commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+
+        System.out.println("Hello world!");
+        String url = System.getProperty("user.dir");
+        System.out.println(url);
+    }
+}
